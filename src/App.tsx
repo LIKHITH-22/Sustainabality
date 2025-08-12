2c11644d76754ac316a6735c6781ab06092fb504
LIKHITH H <141423871+LIKHITH-22@users.noreply.github.com>
2025-08-12 10:44:59 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastProvider } from './components/ui/ToastProvider';
+import { HomePage } from './pages/HomePage';
+import { OrganizationRegistration } from './pages/OrganizationRegistration';
+import { OrganizationQuestionnaire } from './pages/OrganizationQuestionnaire';
+import { OrganizationCertificate } from './pages/OrganizationCertificate';
+import { EmployeeQuestionnaire } from './pages/EmployeeQuestionnaire';
+import { EmployeeCertificate } from './pages/EmployeeCertificate';
+import { PledgeWall } from './pages/PledgeWall';
+
+function App() {
+  return (
+    <ToastProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/organization" element={<OrganizationRegistration />} />
+            <Route path="/organization/questionnaire" element={<OrganizationQuestionnaire />} />
+            <Route path="/organization/certificate" element={<OrganizationCertificate />} />
+            <Route path="/employee" element={<EmployeeQuestionnaire />} />
+            <Route path="/employee/certificate" element={<EmployeeCertificate />} />
+            <Route path="/pledge-wall" element={<PledgeWall />} />
+          </Routes>
+        </div>
+      </Router>
+    </ToastProvider>
+  );
+}
+
+export default App;
